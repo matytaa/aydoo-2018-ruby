a28cae814ea42b51341b8e736c634403be7da0cd
Matias Nievas Soto <matytaa@gmail.com>
2018-05-29 01:50:13 -0300

    implementando impresora solo numeros pares

--- fibo5/spec/ImpresoraListaSoloPares_spec.rb
+++ fibo5/spec/ImpresoraListaSoloPares_spec.rb
@@ -1,12 +1,13 @@
 require 'rspec'
-require_relative '../model/ImpresoraFormatoListaInvertida.rb'
-class ImpresoraFormatoListaInvertidaSpec
-	describe 'ImpresoraFormatoListaInvertida' do
-		impresora = ImpresoraFormatoListaInvertida.new
+require_relative '../model/ImpresoraListaSoloPares.rb'
+class ImpresoraListaSoloParesSpec
+	describe 'ImpresoraListaSoloPares' do
+		impresora = ImpresoraListaSoloPares.new
 		it 'deberia retornar un json con id lista y todos sus elementos' do
 			array_fibo = [0,1,1,2,3,5,8,13]
 			resultado = impresora.imprimir(array_fibo)
-			un_hash = {"lista" => [13,8,5,3,2,1,1,0]}
+			puts resultado
+			un_hash = {"lista" => [0,2,8]}
 			resultado_hash = JSON.parse(resultado)
 			expect(resultado_hash).to include(un_hash)
 		end
